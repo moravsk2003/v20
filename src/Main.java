ead3e5635aace1a91d0a291af2adf47ee5252925
moravsk2003 <126240228+moravsk2003@users.noreply.github.com>
2023-04-06 21:09:05 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-        //1
-        double n = 14.4;
-        if (100 > n && n > 50) {
-            System.out.println("Число " + n + " міститься в проміжку (50; 100)");
-        } else {
-            System.out.println("Число" + n + "не міститься в проміжку (50; 100)");
+//1
+
+
+        System.out.println("1 завдання");
+        for (int n1 = 500; n1 <= 650; n1 += 10) {
+            System.out.println(n1);
         }
+        int n1 = 500;
+        int n2 = 650;
+        while (n1 <= n2) {
+            System.out.println(n1);
+            n1 += 10;
+        }
+        n1 = 500;
+        do {
+            System.out.println(n1);
+            n1 += 10;
+        } while (n1 <= n2);
+        System.out.println("2 завдання");
         //2
-        int n2 = 385;
-        int k1 = n2 / 100;
-        int k2 = (n2 / 10) % 10;
-        int k3 = n2 % 10;
-        if (k1 > k2 && k1 > k3) {
-            System.out.println(k1);
-        } else if (k2 > k1 && k2 > k3) {
-            System.out.println(k2);
-        } else {
-            System.out.println(k3);
+        int a = 2;
+        while (a < 5000) {
+            System.out.println(a);
+            a = 2 * a - 1;
         }
+        System.out.println("3 завдання");
         //3
-        String d = "До низу";
-        int f = 6;
-        if (d =="До гори") {
-            if(f==1) {
-                System.out.println("Ви піднялись на 1 поверх");
-            }else if(f==2||f==3) {
-                System.out.println("Ви піднялись на 3 поверх");
-            }
-            else if(f==4) {
-                System.out.println("Ви піднялись на 4 поверх");
-            }
-            else if(f==5) {
-                System.out.println("Ви піднялись на 5 поверх");
-            }
-            else if(f==6) {
-                System.out.println("Ви піднялись на 6 поверх");
-            }
-            else if(f==7) {
-                System.out.println("Ви піднялись на 7 поверх");
-            }
-            else if(f==8) {
-                System.out.println("Ви піднялись на 8 поверх");
-            }
-            else if(f==9) {
-                System.out.println("Ви піднялись на 9 поверх");
-            }
-            else{
-                System.out.println("Ви вказали неіснуючий поверх!");
+        a = 10;
+        for (int i = 10; i > 0; i--) {
+            if (a % i == 0) {
+                System.out.println(i);
             }
         }
-        else if(d=="До низу"){
-            if(f==9) {
-                System.out.println("Ви спустились на 9 поверх");
-            }
-            else if(f==8) {
-                System.out.println("Ви спустились на 8 поверх");
-            }
-            else if(f==7) {
-                System.out.println("Ви спустились на 7 поверх");
-            }
-            else if(f==6) {
-                System.out.println("Ви спустились на 8 поверх");
-            }
-            else if(f==5) {
-                System.out.println("Ви спустились на 7 поверх");
-            }
-            else if(f==4) {
-                System.out.println("Ви спустились на 8 поверх");
-            }
-            else if(f==3) {
-                System.out.println("Ви спустились на 7 поверх");
-            }
-            else if(f==2&&f==1) {
-                System.out.println("Ви спустились на 1 поверх");
-            }
-            else{
-                System.out.println("Ви вказали неіснуючий поверх!");
+        System.out.println("4 завдання");
+        //4
+        a = 1;
+        for (int i = 1; i <= 10; i++) {
+            a *= i;
+        }
+        System.out.println(a);
+        a = 1;
+        n1 = 1;
+        while (n1 <= 10) {
+            a *= n1;
+            n1++;
+        }
+        System.out.println(a);
+        System.out.println("5 завдання");
+        //5
+        int s=0;
+        for (int t = 0; t < 24; t++) {
+            for (int t1 = 0; t1 < 60; t1++) {
+                if (t % 10 == t1 / 10 && t / 10 == t1 % 10) {
+                    s++;
+                }
             }
         }
-
-//4
-        String ff = "+";
-        switch (ff){
-            case "+":
-            case "OK":
-            case "Так":
-            case "Yes":
-            case "Y":
-            case "Ok":
-                System.out.println("«Я погоджуюсь!»");
-                break;
-            case "-":
-            case "N":
-            case "Ні":
-            case "NO":
-            case "No":
-                System.out.println("«Я відмовляють!»");
-                break;
+        System.out.println(s);
+        System.out.println("6 завдання");
+        //6
+        s = 0;
+        for (int t = 1; t < 999999; t++) {
+            int k = t / 100000 + (t / 10000) % 10 + (t / 1000) % 10;
+            int k1 = t % 10 + (t / 10) % 10 + (t / 100) % 10;
+            if (k1 == k) {
+                s++;
+            }
         }
-
-
+        System.out.println(s);
     }
 }
+
+
